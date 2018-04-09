14d16f050d14981004f8a9eb54c2961b11805f2d
TanyaRoot <kofeneformat@gmail.com>
2018-04-09 16:20:08 +0300

    test-1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hi!')
+const areaLog = document.getElementById("log");
+const inpBtn = document.getElementById("inpBtn");
+// areaLog.innerHTML = "come on!"
+
+document.addEventListener('DOMContentLoaded', () => {
+  inpBtn.onclick = function() {
+    let newText = document.getElementById("inp").value;
+    areaLog.innerHTML += "<p>"+ newText +"</p>";
+    document.getElementById("inp").value = "";
+  }
+});
